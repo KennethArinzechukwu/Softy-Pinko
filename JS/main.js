e20a669eedeb2ad2429a653892dd07315d0a2d13
Kennth Arinzechukwu <oursureplugtechnologies@gmail.com>
2022-06-29 04:30:56 -0700

    completed the site

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+let typed = new Typed('.typing-effect',{
+    strings: [
+      "Brand",
+      "Business",
+      "Start Up",
+    ], backSpeed: "9", typeSpeed:"9", loop:true
+  })
+  
