d280faa09f4e14bf1c62126ae4ccc712eae25468
Kennth Arinzechukwu <oursureplugtechnologies@gmail.com>
2022-07-04 07:38:54 -0700

    final commit

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,48 @@
-$('.counter').counterUp({
-    delay: 10,
-    time: 1000
+AOS.init();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
 });
 
-let typed = new Typed('.typing-effect',{
-    strings: [
-      "Brand",
+$('.counter').counterUp({
+    delay: 15,
+    time: 2000
+});
+
+
+
+// used to initialize the typing effect on the website
+const typed = new Typed(".text-slider", {
+  strings: [
       "Business",
+      "Brand", 
       "Start Up",
-    ], backSpeed: "9", typeSpeed:"9", loop:true
-  })
+  ], 
+  backSpeed:50, 
+  typeSpeed:10,
+  backDelay:90,
+  loop:true
+})
+  
   
